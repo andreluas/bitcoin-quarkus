c7458f6ae7c9fcdabe619c6a8ad80a64dfe5b4a6
Andre Almeida <andreluas.k@gmail.com>
2022-05-15 19:25:41 -0300

    database connection

--- src/main/java/br/com/alura/health/DatabaseConnectionHealthCheck.java
+++ src/main/java/br/com/alura/health/DatabaseConnectionHealthCheck.java
@@ -2,17 +2,39 @@ package br.com.alura.health;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
 
 @Readiness // verifica se o container está pronto para lidar com solicitações
 @ApplicationScoped
 public class DatabaseConnectionHealthCheck implements HealthCheck {
 
+    @ConfigProperty(name = "database.up", defaultValue = "true")
+    private boolean databaseUp;
+
     @Override
     public HealthCheckResponse call() {
-       return HealthCheckResponse.up("Database connection health check");
+
+        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Database connection health check");
+
+        try {
+            simulateDatabaseConnectionVerification();
+            responseBuilder.up();
+        } catch (IllegalStateException e) {
+            // não acessou o database
+            responseBuilder.down();
+        }
+
+        return responseBuilder.build();
     }
-    
+
+    private void simulateDatabaseConnectionVerification() {
+        if (!databaseUp) {
+            throw new IllegalStateException("Cannot contact database");
+        }
+    }
+
 }
