94fbf50119874a2472976eea66a234e2d220e9a2
Andre Almeida <andreluas.k@gmail.com>
2022-05-15 19:25:33 -0300

    startup health check

---
+++ src/main/java/br/com/alura/health/StartupHealthCheck.java
@@ -0,0 +1,18 @@
+package br.com.alura.health;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Startup;
+
+@Startup
+@ApplicationScoped
+public class StartupHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Startup health check");
+    }
+
+}
